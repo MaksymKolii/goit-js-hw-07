b6e16c9cf5bc0823632c865f55509aa557c768b3
Maksym Kolii <maksymkolii@gmail.com>
2022-09-16 23:47:16 +0200

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
 /* Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
 Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
@@ -12,3 +12,68 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryDivRef = document.querySelector(".gallery");
+
+// function makeGalleryRowsMarkup({ preview, original, description }) {
+//   return `<div class="gallery__item">
+//     <a class="gallery__link" href="${original}">
+//       <img
+//         class="gallery__image"
+//         src="${preview}"
+//         data-source="${original}"
+//         alt="${description}"
+//       />
+//     </a>
+//   </div>
+//   `;
+// };
+
+// const makeGaleryRows = galleryItems.map(makeGalleryRowsMarkup).join("");
+
+function makeGalleryRowsMarkup(items) {
+    return items
+    .map(item => 
+
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                 <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+            </a>
+        </div>`
+    )
+    .join('')
+    
+  };
+  
+const makeGaleryRows = makeGalleryRowsMarkup(galleryItems);
+
+console.log(makeGaleryRows);
+
+galleryDivRef.insertAdjacentHTML("beforeend", makeGaleryRows);
+
+galleryDivRef.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const clickedImage = e.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${clickedImage}" width="800" height="600">`);
+
+  instance.show();
+
+  galleryDivRef.addEventListener("keydown", (e) => {
+
+    if (e.key === "Escape") {
+
+        instance.close();
+    }
+  });
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryElRef = document.querySelector(".gallery");
+
+function makeGalleryRowsMarkup(items)  {
+
+  return  items
+    .map(item =>
+
+        `<a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>`
+    )
+    .join('')
+
+}
+
+const makeGaleryRows = makeGalleryRowsMarkup(galleryItems);
+console.log(makeGaleryRows);
+
+ galleryElRef.insertAdjacentHTML("beforeend", makeGaleryRows);
+
+
+
+//const lightbox = 
+
+new SimpleLightbox('.gallery a',{
+    
+    captionDelay: 7250, 
+    navText: ['←','→'],
+    closeText:	'esolC',
+    animationSpeed:	250,
+    captionType: 'attr',
+    captionPosition:'bottom',
+
+});
