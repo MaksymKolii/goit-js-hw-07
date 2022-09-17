05ccb31172c677f9af831510f06a52f56702f25e
Maksym Kolii <maksymkolii@gmail.com>
2022-09-17 18:26:56 +0200

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,79 +1,51 @@
 import { galleryItems } from "./gallery-items.js";
 
-/* Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
-Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-Реализация делегирования на div.gallery и получение url большого изображения.
-Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на 
-минифицированные (.min) файлы библиотеки.
-Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
-Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением
- из примеров библиотеки basicLightbox. */
-
-// Change code below this line
-
 console.log(galleryItems);
 
 const galleryDivRef = document.querySelector(".gallery");
 
-// function makeGalleryRowsMarkup({ preview, original, description }) {
-//   return `<div class="gallery__item">
-//     <a class="gallery__link" href="${original}">
-//       <img
-//         class="gallery__image"
-//         src="${preview}"
-//         data-source="${original}"
-//         alt="${description}"
-//       />
-//     </a>
-//   </div>
-//   `;
-// };
-
-// const makeGaleryRows = galleryItems.map(makeGalleryRowsMarkup).join("");
-
-function makeGalleryRowsMarkup(items) {
-    return items
-    .map(item => 
-
-        `<div class="gallery__item">
-            <a class="gallery__link" href="${item.original}">
-                 <img
-                    class="gallery__image"
-                    src="${item.preview}"
-                    data-source="${item.original}"
-                    alt="${item.description}"
-                />
-            </a>
-        </div>`
+function makeGalleryMarkup(object) {
+  return object
+    .map(
+      ({ original, preview, description }) => `<div class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                   <img
+                      class="gallery__image"
+                      src="${preview}"
+                      data-source="${original}"
+                      alt="${description}"
+                  />
+              </a>
+          </div>`
     )
-    .join('')
-    
-  };
-  
-const makeGaleryRows = makeGalleryRowsMarkup(galleryItems);
+    .join("");
+}
 
-console.log(makeGaleryRows);
+galleryDivRef.innerHTML = makeGalleryMarkup(galleryItems);
 
-galleryDivRef.insertAdjacentHTML("beforeend", makeGaleryRows);
+galleryDivRef.addEventListener("click", onGalleryClick);
 
-galleryDivRef.addEventListener("click", (e) => {
+function onGalleryClick(e) {
   e.preventDefault();
-  if (e.target.nodeName !== "IMG") {
-    return;
-  }
 
-  const clickedImage = e.target.dataset.source;
+  if (e.target.nodeName !== "IMG") return;
 
-  const instance = basicLightbox.create(`
-    <img src="${clickedImage}" width="800" height="600">`);
+  const elTarget = e.target.dataset.source;
+  const instance = basicLightbox.create(
+    `
+  <img src="${elTarget}" width="800" height="600">
+`,
+    {
+      onShow: () => document.addEventListener("keydown", onEscBtnPress),
+      onClose: () => document.removeEventListener("keydown", onEscBtnPress),
+    }
+  );
 
   instance.show();
 
-  galleryDivRef.addEventListener("keydown", (e) => {
-
+  function onEscBtnPress(e) {
     if (e.key === "Escape") {
-
-        instance.close();
+      instance.close();
     }
-  });
-});
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,39 +1,25 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
-
-console.log(galleryItems);
 
 const galleryElRef = document.querySelector(".gallery");
 
-function makeGalleryRowsMarkup(items)  {
-
-  return  items
-    .map(item =>
-
-        `<a class="gallery__item" href="${item.original}">
-            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-        </a>`
+function makeGalleryMarkup(object) {
+  return object
+    .map(
+      ({ original, preview, description }) => `<li class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                   <img
+                      class="gallery__image"
+                      src="${preview}"
+                      alt="${description}"
+                  />
+              </a>
+          </li>`
     )
-    .join('')
-
+    .join("");
 }
+galleryElRef.insertAdjacentHTML("beforeend", makeGalleryMarkup(galleryItems));
 
-const makeGaleryRows = makeGalleryRowsMarkup(galleryItems);
-console.log(makeGaleryRows);
-
- galleryElRef.insertAdjacentHTML("beforeend", makeGaleryRows);
-
-
-
-//const lightbox = 
-
-new SimpleLightbox('.gallery a',{
-    
-    captionDelay: 7250, 
-    //navText: ['←','→'],
-    //closeText:	'esolC',
-    //animationSpeed:	250,
-    captionType: 'attr',
-    captionPosition:'bottom',
-
+new SimpleLightbox(".gallery gallery__link", {
+  captionsData: "alt",
+  captionDelay: 250,
 });
